1fb2884ffbe29a4aef7b0d5cb9f07a719abec89a
Manish Shukla <manish.shukla86@rediff.com>
2023-04-24 04:57:22 +0530

    Implemented the Edit button

--- gui.py
+++ gui.py
@@ -4,11 +4,13 @@ import PySimpleGUI as gui
 label = gui.Text("Type in a to-do")       ## This is label to the text box
 input_box = gui.InputText(key='todo')    ## This is suggestion text appear when we hover cursor on text-box in window
 add_button = gui.Button("Add")       ## To add button in the window
-#show_button
+list_box = gui.Listbox(values=functions.get_todo(), key="items",
+                       enable_events=True, size=[45, 10])
+edit_button = gui.Button("Edit")
 
 ## Windows method to create a GUI window, So we use read/close methods to open/close the window
 window = gui.Window("My To-Do App",
-                    layout=[[label, input_box, add_button]],
+                    layout=[[label, input_box, add_button], [list_box, edit_button]],
                     font=('Helvetica', 20))
 
 while True:
@@ -21,8 +23,21 @@ while True:
             new_todo = values['todo'] + "\n"
             todos.append(new_todo)
             functions.write_todos(todos)
+            window["items"].update(values=todos)
+        case "Edit":
+            todo_to_edit = values["items"][0]
+            new_todo = values["todo"] + "\n"
+
+            todos = functions.get_todo()
+            index = todos.index(todo_to_edit)
+            todos[index] = new_todo
+            functions.write_todos(todos)
+            window["items"].update(values=todos)      ### Here, we are updating todo list to display in edit box, we selected "items" key from windows refresh/update that with new todos list
+        case "items":
+            window["todo"].update(value=values["items"][0])
         case gui.WIN_CLOSED:
             break
+
 
 window.close()
 
